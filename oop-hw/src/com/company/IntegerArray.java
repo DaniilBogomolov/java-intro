ae36930c901756a5180a45c2368dc8fd499b0d68
Даниил Богомолов <bogomolov.daniil.99@gmail.com>
2021-03-23 20:27:10 +0300

    hw

--- oop-hw/src/com/company/IntegerArray.java
+++ oop-hw/src/com/company/IntegerArray.java
@@ -5,7 +5,7 @@ public interface IntegerArray {
     void addAll(int[] elements);
     void remove(int index);
     int get(int index);
-    int indexOf(int element);
+    int indexOf(int element); // если отсортирован -> бинарным поиском
     int length();
     void reverse();
     void sort();
--- oop-hw/src/com/company/IntegerArrayImpl.java
+++ oop-hw/src/com/company/IntegerArrayImpl.java
@@ -6,10 +6,18 @@ public class IntegerArrayImpl implements IntegerArray {
     private int count;
     private boolean isSorted; // внимательно следите за значением поля
 
+    public IntegerArrayImpl() {
+        this(10);
+    }
+
     public IntegerArrayImpl(int defaultCapacity) {
         elements = new int[defaultCapacity];
         count = 0;
         isSorted = true;
+    }
+
+    private void ensureCapacity() {
+//        ... увеличить массив в 2 раза
     }
 
 }
